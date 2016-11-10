071581246879dac65eb91ca9e61ceb3c0bb9197b
nolan66 <ngallant.sh@mysummitps.org>
2016-11-10 19:31:05 +0000

    update

--- myGame/story.js
+++ myGame/story.js
@@ -2,28 +2,39 @@
 
 
 
-game_state.main = function () {};
+game_state.story = function() {};
 game_state.story.prototype = {
 
 
-function preload() {
-
-
-},
-
-
-function create() {
-
+preload: function () {
+game.load.image('sky', 'assets/sky.png');
 
 },
 
 
-function update() {
+create: function () {
+
+game.add.sprite(0, 0, 'sky');
 
 
+this.storyText = game.add.text(225, game.world.height / 3, 'Welcome to Paradise\n A.K.A. Hell\nyour mission is to complete\nanything and everything\nput infront of you\nPress down to begin!',{
+            fill: '#ffffff'
+        });
+this.cursors = game.input.keyboard.createCursorKeys();
+this.down.inputEnabled = true;
+var gameNum = 1;
 },
+
+update: function() {
+if (this.cursors.down.isDown) {
+       game.state.start('main');
+}
 
 
 }
-game.state.add('main', game_state.main);
-game.state.start('main');
+
+    
+},
+
+game.state.add('story', game_state.story);
+game.state.start('story');
